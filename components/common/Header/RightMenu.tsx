353bd220ae09d5811ce14060760025b2ad6a13d7
Chodongdong <117080299+Chodongdong@users.noreply.github.com>
2025-11-20 23:03:05 +0900

    feat: 공통 컴포넌트 Header 제작 (#6) (#110)
    * feat/#6-common-component-header
    
    * PR 반영 수정 및 searchbar 돋보기 아이콘 클릭시 엔터와 같은 역할 부여
    
    * Delete app/page.tsx
    
    testfile 제거
    
    ---------
    
    Co-authored-by: DongHyun <cdhppph@gamil.com>

---
+++ components/common/Header/RightMenu.tsx
@@ -0,0 +1,82 @@
+"use client";
+
+import Link from "next/link";
+import BellIcon from "@/src/assets/vector.svg";
+
+type HeaderState = "owner" | "worker" | "guest";
+
+interface RightMenuProps {
+  state: HeaderState;
+  onLogout: () => void;
+  onOpenNotification: () => void;
+}
+
+const RightMenu = ({ state, onLogout, onOpenNotification }: RightMenuProps) => {
+  return (
+    <div className="flex items-center gap-6 text-gray-700">
+      {/* 사장님 로그인 */}
+      {state === "owner" && (
+        <>
+          <Link
+            href="/storeInfoDetail"
+            className="text-sm hover:text-black transition"
+          >
+            내 가게
+          </Link>
+
+          <button
+            onClick={onLogout}
+            className="text-sm hover:text-black transition"
+          >
+            로그아웃
+          </button>
+
+          <button
+            onClick={onOpenNotification}
+            className="hover:opacity-70 transition"
+          >
+            <BellIcon className="w-6 h-6" />
+          </button>
+        </>
+      )}
+
+      {/* 알바 로그인 */}
+      {state === "worker" && (
+        <>
+          <Link href="/profile" className="text-sm hover:text-black transition">
+            내 프로필
+          </Link>
+
+          <button
+            onClick={onLogout}
+            className="text-sm hover:text-black transition"
+          >
+            로그아웃
+          </button>
+
+          <button
+            onClick={onOpenNotification}
+            className="hover:opacity-70 transition"
+          >
+            <BellIcon className="w-6 h-6" />
+          </button>
+        </>
+      )}
+
+      {/* 비로그인 */}
+      {state === "guest" && (
+        <>
+          <Link href="/login" className="text-sm hover:text-black transition">
+            로그인
+          </Link>
+
+          <Link href="/signup" className="text-sm hover:text-black transition">
+            회원가입
+          </Link>
+        </>
+      )}
+    </div>
+  );
+};
+
+export default RightMenu;
