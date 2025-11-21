d5531cb7c5c976df6b3dbaee9cb40de12e8eb00d
jerryko570 <jerry.narae@gmail.com>
2025-11-21 10:28:35 +0900

    chore: axios 설치 및 api 인스턴스 설정 (#32)
    * chore: axios 설치 및 api 인스턴스 설정 (#32)
    
    * chore/#32-setup-axios-instance-merge
    
    * chore: axios 인스턴스 타입스크립트 적용 #32
    
    * chore: axios 인스터스 불필요 코드 삭제 (#32)

---
+++ src/lib/axios.ts
@@ -0,0 +1,39 @@
+import axios, { AxiosInstance, InternalAxiosRequestConfig , AxiosError, AxiosResponse } from 'axios';
+
+// axios 인스턴스 생성
+export const api : AxiosInstance = axios.create({
+  baseURL: process.env.NEXT_PUBLIC_API_URL,
+  timeout: 10000,
+  headers: {
+    'Content-Type': 'application/json',
+  },
+});
+
+// 요청 인터셉터 (토큰 자동 추가)
+api.interceptors.request.use(
+  (config : InternalAxiosRequestConfig) => {
+    if (typeof window !== 'undefined') {
+    const token = localStorage.getItem('token');
+    if (token) {
+      config.headers = config.headers || {};
+      config.headers.Authorization = `Bearer ${token}`;
+    }
+    }
+    return config;
+  },
+  (error : AxiosError) => Promise.reject(error)
+);
+
+// 응답 인터셉터 (공통 에러 처리)
+api.interceptors.response.use(
+  (res : AxiosResponse) => res,
+  (err : AxiosError) => {
+    if (err.response?.status === 401) {
+      if (typeof window !== 'undefined') {
+      localStorage.removeItem('token');
+      window.location.href = '/login';
+      }
+    }
+    return Promise.reject(err);
+  }
+);
