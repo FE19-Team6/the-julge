cda74a010b8547841d8a1d86029e75a1b4e9e832
지니 <117137463+yujinimda@users.noreply.github.com>
2025-11-22 00:32:57 +0900

    refactor: as패턴에서 button 전용 컴포넌트로 수정 (#114)
    * refactor: uncomment button clsx (#114)
    
    * refactor: 최신 css 반영 (#114)
    
    * refactor: as 패턴에서 button 전용 컴포넌트로 변경 (#114)

--- components/common/Button/Button.tsx
+++ components/common/Button/Button.tsx
@@ -1,32 +1,34 @@
 "use client";
 
-//import clsx from "clsx";
+import clsx from "clsx";
 import { ComponentPropsWithoutRef, ElementType, ReactNode } from "react";
-//import { BUTTON_VARIANTS, BUTTON_SIZES } from "./buttonVariants";
+import { BUTTON_VARIANTS, BUTTON_SIZES, BUTTON_BASE } from "./buttonVariants";
 
-export type ButtonProps<T extends ElementType> = {
-  as?: T;
+export type ButtonProps = ComponentPropsWithoutRef<"button"> & {
   children: ReactNode;
   variant?: "primary" | "outline" | "disabled";
-  size?: "lg" | "md" | "sm";
-} & ComponentPropsWithoutRef<T>;
+  size?: "lg" | "md" | "sm" | "full";
+};
 
-const Button = <T extends ElementType = "button">({
-  as,
+const Button = ({
   children,
   variant = "primary",
   size = "md",
   className,
   ...rest
-}: ButtonProps<T>) => {
-  const Component = as || "button";
+}: ButtonProps) => {
   return (
-    <Component
+    <button
       {...rest}
-      //className={clsx(BUTTON_VARIANTS[variant], BUTTON_SIZES[size], className)}
+      className={clsx(
+        BUTTON_BASE,
+        BUTTON_VARIANTS[variant],
+        BUTTON_SIZES[size],
+        className
+      )}
     >
       {children}
-    </Component>
+    </button>
   );
 };
 
--- components/common/Button/buttonVariants.ts
+++ components/common/Button/buttonVariants.ts
@@ -4,26 +4,25 @@ export const BUTTON_BASE = `
   rounded-[6px]
   text-center
   transition-colors duration-150
-  cursor-pointer
 `;
 
 /** 버튼 스타일 종류 */
 // "primary" | "outline" | "disabled"
 export const BUTTON_VARIANTS = {
   primary: `
-    bg-primary-900 text-white
-    hover:bg-white hover:text-primary-900 hover:border hover:border-primary-900
-    active:bg-white active:text-primary-900 active:border active:border-primary-900
-    focus:bg-white focus:text-primary-900 focus:border focus:border-primary-900
+    bg-red-50 text-white cursor-pointer
+    hover:bg-white hover:text-red-50 hover:border hover:border-red-50 
+    active:bg-white active:text-red-50 active:border active:border-red-50
+    focus:bg-white focus:text-red-50 focus:border focus:border-red-50
   `,
   outline: `
-    bg-white border border-primary-900 text-primary-900
-    hover:bg-primary-900 hover:text-white
-    active:bg-primary-900 active:text-white
-    focus:bg-primary-900 focus:text-white
+    bg-white border border-red-50 text-red-50 cursor-pointer
+    hover:bg-red-50 hover:text-white
+    active:bg-red-50 active:text-white
+    focus:bg-red-50 focus:text-white
   `,
   disabled: `
-    bg-gray-600 text-white cursor-not-allowed
+    bg-gray-40 text-white cursor-not-allowed
   `,
 } as const;
 
@@ -33,15 +32,15 @@ export type ButtonVariant = keyof typeof BUTTON_VARIANTS;
 // "sm" | "md" | "lg" | "full"
 export const BUTTON_SIZES = {
   sm: `
-    w-20 h-8 px-3
+    w-[82px] h-8 px-3
     text-[12px] font-[500]
   `,
   md: `
-    w-27 h-9 px-4
+    w-[108px] h-9 px-4
     text-[14px] font-[500]
   `,
   lg: `
-    w-87 h-12 px-5
+    w-[350px] h-12 px-5
     text-[16px] font-[500]
   `,
   full: `
