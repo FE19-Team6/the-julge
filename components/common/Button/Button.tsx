5c2f941c6415b70a08751546ac1f7dcc8b121ca2
yujinimda <s0912135@gmail.com>
2025-11-21 16:56:07 +0900

    refactor: uncomment button clsx (#114)

--- components/common/Button/Button.tsx
+++ components/common/Button/Button.tsx
@@ -1,14 +1,14 @@
 "use client";
 
-//import clsx from "clsx";
+import clsx from "clsx";
 import { ComponentPropsWithoutRef, ElementType, ReactNode } from "react";
-//import { BUTTON_VARIANTS, BUTTON_SIZES } from "./buttonVariants";
+import { BUTTON_VARIANTS, BUTTON_SIZES, BUTTON_BASE } from "./buttonVariants";
 
 export type ButtonProps<T extends ElementType> = {
   as?: T;
   children: ReactNode;
   variant?: "primary" | "outline" | "disabled";
-  size?: "lg" | "md" | "sm";
+  size?: "lg" | "md" | "sm" | "full";
 } & ComponentPropsWithoutRef<T>;
 
 const Button = <T extends ElementType = "button">({
@@ -23,7 +23,12 @@ const Button = <T extends ElementType = "button">({
   return (
     <Component
       {...rest}
-      //className={clsx(BUTTON_VARIANTS[variant], BUTTON_SIZES[size], className)}
+      className={clsx(
+        BUTTON_BASE,
+        BUTTON_VARIANTS[variant],
+        BUTTON_SIZES[size],
+        className
+      )}
     >
       {children}
     </Component>
--- components/common/Button/buttonVariants.ts
+++ components/common/Button/buttonVariants.ts
@@ -4,23 +4,22 @@ export const BUTTON_BASE = `
   rounded-[6px]
   text-center
   transition-colors duration-150
-  cursor-pointer
 `;
 
 /** 버튼 스타일 종류 */
 // "primary" | "outline" | "disabled"
 export const BUTTON_VARIANTS = {
   primary: `
-    bg-primary-900 text-white
-    hover:bg-white hover:text-primary-900 hover:border hover:border-primary-900
-    active:bg-white active:text-primary-900 active:border active:border-primary-900
-    focus:bg-white focus:text-primary-900 focus:border focus:border-primary-900
+    bg-primary-700 text-white cursor-pointer
+    hover:bg-white hover:text-primary-700 hover:border hover:border-primary-700 
+    active:bg-white active:text-primary-700 active:border active:border-primary-700
+    focus:bg-white focus:text-primary-700 focus:border focus:border-primary-700
   `,
   outline: `
-    bg-white border border-primary-900 text-primary-900
-    hover:bg-primary-900 hover:text-white
-    active:bg-primary-900 active:text-white
-    focus:bg-primary-900 focus:text-white
+    bg-white border border-primary-700 text-primary-700 cursor-pointer
+    hover:bg-primary-700 hover:text-white
+    active:bg-primary-700 active:text-white
+    focus:bg-primary-700 focus:text-white
   `,
   disabled: `
     bg-gray-600 text-white cursor-not-allowed
@@ -33,15 +32,15 @@ export type ButtonVariant = keyof typeof BUTTON_VARIANTS;
 // "sm" | "md" | "lg" | "full"
 export const BUTTON_SIZES = {
   sm: `
-    w-20 h-8 px-3
+    w-[82px] h-8 px-3
     text-[12px] font-[500]
   `,
   md: `
-    w-27 h-9 px-4
+    w-[108px] h-9 px-4
     text-[14px] font-[500]
   `,
   lg: `
-    w-87 h-12 px-5
+    w-[350px] h-12 px-5
     text-[16px] font-[500]
   `,
   full: `
