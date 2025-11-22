38e778f816fd3f38bf5d4d2da227d0a6cd246d01
jerryko570 <jerry.narae@gmail.com>
2025-11-23 07:41:17 +0900

    fix: SearchInput 파일 복구 및 코드리뷰 반영하여 접근성 개선 및 코드수정

--- components/common/SearchInput/SearchInput.tsx
+++ components/common/SearchInput/SearchInput.tsx
@@ -14,6 +14,12 @@ interface SearchInputProps {
 
 type SearchSize = "web" | "tablet" | "mobile";
 
+const SEARCH_SIZE = {
+  web: "w-[450px] h-[40px] text-body2",
+  tablet: "w-[360px] h-[38px] text-body2",
+  mobile: "w-[335px] h-[36px] text-description",
+} as const;
+
 export default function SearchInput( 
     {value, onChange, onSearch, placeholder="가게 이름으로 찾아보세요", size="web"} : SearchInputProps ) {
   const [isFocused, setIsFocused] = useState(false);
@@ -24,14 +30,9 @@ export default function SearchInput(
     }
   };
 
-  const SEARCH_SIZE = {
-    web: "w-[450px] h-[40px] text-body2",
-    tablet: "w-[360px] h-[38px] text-body2",
-    mobile: "w-[335px] h-[36px] text-description",
-  } as const;
-
   return (
     <div className="relative w-full max-w-[450px]">
+      {/* 인풋 영역 */}
       <input
         value={value}
         onChange={(e) => onChange(e.target.value)}
@@ -42,7 +43,7 @@ export default function SearchInput(
         className={clsx(
         "bg-gray-10 rounded-[8px]",
         "pl-12 pr-4 transition-all",
-        "placeholder:text-gray-40 text-[14px] text-body2",
+        "placeholder:text-gray-40 text-body2",
         "outline-none border", 
         
         SEARCH_SIZE[size],
@@ -53,12 +54,21 @@ export default function SearchInput(
         )}
       />
 
-        <SearchIcon
-            className={clsx (
-                "absolute left-4 top-1/2 -translate-y-1/2",
-                "text-gray-40"
-            )}
-        />
+        {/* 돋보기 아이콘 */}
+        <div
+          role="button"
+          tabIndex={0}
+          onClick={() => onSearch(value)}
+          onKeyDown={(e) => {
+            if (e.key === "Enter") onSearch(value);
+          }}
+          className={clsx(
+            "absolute left-4 top-1/2 -translate-y-1/2",
+            "cursor-pointer"
+          )}
+         >
+          <SearchIcon className="text-gray-40" />
+        </div>
     </div>
   );
 }
