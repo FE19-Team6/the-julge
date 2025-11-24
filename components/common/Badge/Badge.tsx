e1081dccd014421c67b65546b9b236cc389874e6
DongHyun <cdhppph@gamil.com>
2025-11-24 18:30:12 +0900

    feat: pr-반영

--- components/common/Badge/Badge.tsx
+++ components/common/Badge/Badge.tsx
@@ -1,5 +1,3 @@
-"use client";
-
 import clsx from "clsx";
 import { ComponentPropsWithoutRef, ReactNode } from "react";
 import ArrowUpIcon from "@/src/assets/arrow-up.svg";
--- components/common/Badge/BadgeClient.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import Badge, { BadgeProps } from "./Badge";
-
-export default function BadgeClient(props: BadgeProps) {
-  return <Badge {...props} />;
-}
