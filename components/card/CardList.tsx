4ab8462c0b16e67450c65be261d764e16f48f7bf
jerryko570 <jerry.narae@gmail.com>
2025-11-24 10:34:20 +0900

    feat: CardList 컴포넌트 추가

--- components/card/CardList.tsx
+++ components/card/CardList.tsx
@@ -5,7 +5,7 @@ type CardListProps = {
   items: CardProps[];
 };
 
-// 여러개의 Card를 받아 그리드 형태로 렌더링하는 리스트 레이아웃
+// 여러개의 Card를 받아 그리드 형태로 렌더링하는 리스트 레이아웃 
 export default function CardList({ items}: CardListProps) {
   return (
       <div className="max-w-[964px] mx-auto">
