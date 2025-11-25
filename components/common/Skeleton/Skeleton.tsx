e8e4987b36253c89fe0bfef1c88f5e97016508bf
jerryko570 <jerry.narae@gmail.com>
2025-11-25 14:45:12 +0900

    refactor: Skeleton 컴포넌트 함수명 변경 및 반응형 코드 추가

--- components/common/Skeleton/Skeleton.tsx
+++ components/common/Skeleton/Skeleton.tsx
@@ -1,15 +1,23 @@
-export default function CardSkeleton() {
+export default function Skeleton() {
   return (
-    <div className="w-full border border-gray-20 p-4 rounded-[12px] animate-pulse">
-      {/* 이미지 스켈레톤 */}
-      <div className="w-full rounded-xl bg-gray-20 h-[100px] md:h-[160px] lg:h-[170px]" />
+    <div className="w-full max-w-[964px] mx-auto">
+      {/* 그리드 레이아웃 */}
+      <div className="grid grid-cols-2 lg:grid-cols-3 gap-6">
+        
+        {/* 단일 카드 스켈레톤 */}
+        <div className="w-full border border-gray-20 p-4 rounded-[12px] animate-pulse">
+          {/* 이미지 스켈레톤 */}
+          <div className="w-full rounded-xl bg-gray-20 h-[100px] md:h-[160px] lg:h-[170px]" />
 
-      {/* 텍스트 스켈레톤 */}
-      <div className="mt-4 space-y-3">
-        <div className="h-4 w-1/2 bg-gray-20 rounded" />
-        <div className="h-4 w-3/4 bg-gray-20 rounded" />
-        <div className="h-4 w-2/3 bg-gray-20 rounded" />
-        <div className="h-4 w-1/3 bg-gray-20 rounded" />
+          {/* 텍스트 스켈레톤 */}
+          <div className="mt-4 space-y-3">
+            <div className="h-4 w-1/2 bg-gray-20 rounded" />
+            <div className="h-4 w-3/4 bg-gray-20 rounded" />
+            <div className="h-4 w-2/3 bg-gray-20 rounded" />
+            <div className="h-4 w-1/3 bg-gray-20 rounded" />
+          </div>
+        </div>
+
       </div>
     </div>
   );
