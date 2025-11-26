6e363e82f1b657b37031334d636981ece810b647
DongHyun <cdhppph@gamil.com>
2025-11-26 12:29:26 +0900

    feat: storeinfocreate-layout-create

---
+++ src/app/(main)/mystore/utils/adressOptions.ts
@@ -0,0 +1,9 @@
+import { DropdownOption } from "@/components/common/Dropdown/Dropdown";
+
+export const addressOptions: DropdownOption[] = [
+  { label: "서울시 종로구", value: "서울시 종로구" },
+  { label: "서울시 중구", value: "서울시 중구" },
+  { label: "서울시 용산구", value: "서울시 용산구" },
+  { label: "서울시 성동구", value: "서울시 성동구" },
+  { label: "서울시 광진구", value: "서울시 광진구" },
+];
