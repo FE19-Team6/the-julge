e6564f97b2914052182ef019250df5faada9e26b
jerryko570 <jerry.narae@gmail.com>
2025-11-27 16:42:55 +0900

    fix: signup.ts에 console.log 삭제

--- src/features/auth/services/signup.ts
+++ src/features/auth/services/signup.ts
@@ -4,8 +4,6 @@ import type { SignupPayload } from "@/src/features/auth/type";
 
 // 회원가입 버튼을 누르면 api.post() 실행됨
 export const signup = async (payload: SignupPayload) => {
-    console.log("🚀 Signup through API Route");
-  console.log("Payload:", payload);
 
   const res = await api.post("/users", payload);
   return res.data;
