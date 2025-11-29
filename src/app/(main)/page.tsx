6c85bc2b9481fd6b3a6ce197a8fca40ac553d46a
지니 <117137463+yujinimda@users.noreply.github.com>
2025-11-30 08:00:22 +0900

    feat: 헤더 검색기능 searchParams로 구현 (#72)

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -3,14 +3,28 @@ import { AllJobsSection } from "./../../features/jobs/components/jobList/AllJobs
 import { getUserAddress } from "@/src/lib/utils/getCookies";
 import { noticeService } from "@/src/features/jobs/services/getNotices";
 
-export default async function MainPage() {
+export default async function MainPage({
+  searchParams,
+}: {
+  searchParams: Promise<{ keyword?: string }>;
+}) {
   const address = (await getUserAddress()) ?? "";
+  const params = await searchParams;
+  const keyword = params.keyword;
 
   // 맞춤 공고 (주소 기반)
-  const recommendedNotices = await noticeService.getNotice({ address });
+  const recommendedNotices = await noticeService.getNotice({
+    limit: 3,
+    offset: 0,
+    address,
+  });
 
   // 전체 공고 (기본값)
-  const allNotices = await noticeService.getNotice({ limit: 10, offset: 0 });
+  const allNotices = await noticeService.getNotice({
+    limit: 6,
+    offset: 0,
+    ...(keyword && { keyword }),
+  });
   return (
     <>
       <RecommendedJobsSection initialData={recommendedNotices} />
