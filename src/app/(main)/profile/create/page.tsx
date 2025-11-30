777dcfe4e0b286b810e47d267ed4ec5e7f419ba8
jerryko570 <jerry.narae@gmail.com>
2025-11-30 09:00:42 +0900

    feat: 프로필 Main + Create UI 작업

--- src/app/(main)/profile/create/page.tsx
+++ src/app/(main)/profile/create/page.tsx
@@ -1,3 +1,79 @@
+
+"use client";
+
+import { useState } from "react";
+import Button from "@/src/components/common/Button/Button";
+import ProfileForm from "@/src/features/profile/components/ProfileForm";
+import ProfileModal, {
+  ProfileModalType,
+} from "@/src/features/profile/components/ProfileModal";
+
+import { useProfileForm } from "@/src/features/profile/hooks/useProfileForm";
+import { updateProfile } from "@/src/features/profile/services/updateProfile";
+
 export default function Page() {
-  return <></>;
+  // 프로필 작성 상태 + 핸들러 + 에러
+  const profileForm = useProfileForm();
+
+  // 모달 상태
+  const [isModalOpen, setIsModalOpen] = useState(false);
+  const [modalType, setModalType] = useState<ProfileModalType>("VALIDATION");
+
+  // 등록하기 클릭
+  const handleSubmit = async () => {
+    
+    // 전체 유효성 검사
+    if (!profileForm.isFormValid) {
+      setModalType("VALIDATION");
+      setIsModalOpen(true);
+      return;
+    }
+
+    // API 호출
+    try {
+      await updateProfile(
+        {
+        name: profileForm.name,
+        phone: profileForm.phone,
+        address: profileForm.address,
+        bio: profileForm.bio,
+      });
+
+      setModalType("SUCCESS");
+      setIsModalOpen(true);
+
+    } catch (err) {
+      console.error(err);
+      setModalType("FAIL");
+      setIsModalOpen(true);
+    }
+  };
+
+  return (
+    <>
+      {/* 커스텀 모달 */}
+      <ProfileModal
+        isOpen={isModalOpen}
+        type={modalType}
+        onConfirm={() => setIsModalOpen(false)}
+      />
+
+      {/* 페이지 레이아웃 */}
+      <div className="w-full mt-15">
+      <div className="w-full max-w-[964px] mx-auto px-4 md:px-6 lg:px-0 space-y-8">
+        <h1 className="text-h2">내 프로필</h1>
+
+          {/* 폼 영역 */}
+          <ProfileForm form={profileForm} />
+
+          {/* 버튼 중앙 정렬 */}
+          <div className="w-full flex justify-center mt-6">
+            <Button variant="primary" size="lg" onClick={handleSubmit}>
+              등록하기
+            </Button>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 }
--- src/app/(main)/profile/page.tsx
+++ src/app/(main)/profile/page.tsx
@@ -1,3 +1,24 @@
+import Button from "@/src/components/common/Button/Button"
+import Link from "next/link"
+
+// Profile Main Page
+// 사용자가 자신의 프로필을 관리할 수 있는 시작 페이지
+// 프로필이 없을 때 등록 안내 및 등록 버튼을 렌더링함
 export default function Page() {
-  return <></>;
+  return (
+    <div className="w-full mt-15">
+        <div className="w-full max-w-[964px] mx-auto px-4 md:px-6 lg:px-0 space-y-8">
+          <h1 className="text-h2">내 프로필</h1>
+          <div className="flex items-center justify-center h-[217px] border border-gray-20">
+            <div className="flex flex-col items-center gap-5">
+              <p> 내 프로필을 등록하고 원하는 가게에 지원해 보세요! </p>
+              <Link href="/profile/create">
+                <Button variant="primary" size="lg">내 프로필 등록하기</Button>
+              </Link>
+            </div>
+          </div>
+        </div>
+      </div>
+    
+  )
 }
