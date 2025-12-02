d200de15008ddd11998a9a3480a33ad6bbc8fa85
지니 <117137463+yujinimda@users.noreply.github.com>
2025-12-02 11:02:41 +0900

    chore: vercel analytics (#286)

--- src/app/(main)/layout.tsx
+++ src/app/(main)/layout.tsx
@@ -1,6 +1,7 @@
 import { getUserType } from "@/src/lib/utils/getCookies";
 import Header from "@/src/components/common/Header/Header";
 import { Footer } from "./../../components/common/Footer/Footer";
+import { Analytics } from "@vercel/analytics/next";
 
 //Layout은 Server Component이고 Server Component는 async 함수로 만들 수 있습니다.
 export default async function MainLayout({
@@ -16,6 +17,7 @@ export default async function MainLayout({
       <main className="flex-1 w-full mx-auto">{children}</main>
       {/* 푸터 완성되면 아래 지우고 컴포넌트 교체 */}
       <Footer />
+      <Analytics />
     </div>
   );
 }
